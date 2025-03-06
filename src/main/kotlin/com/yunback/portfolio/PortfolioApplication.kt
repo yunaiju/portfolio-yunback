a5f1fcd23c4970df78789163d0b53a9ca9339946
yunaiju <hyejoo04@naver.com>
2025-03-06 17:27:10 +0900

    Git과 GitHub

---
+++ src/main/kotlin/com/yunback/portfolio/PortfolioApplication.kt
@@ -0,0 +1,11 @@
+package com.yunback.portfolio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PortfolioApplication
+
+fun main(args: Array<String>) {
+	runApplication<PortfolioApplication>(*args)
+}
